1b54c91b9cd2785c88a67d78543ff2d491edf4dd
Pj Contas <pjassinaturas@gmail.com>
2026-02-19 00:39:22 +0000

    fix: active tab type for auth forms

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,13 +54,11 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
+        Chevron: ({ className, ...props }) => (
           <ChevronRight className={cn("h-4 w-4", className)} {...props} />
         ),
       }}
+      
       {...props}
     />
   )
